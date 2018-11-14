4dfbf5e250c72de62066cbf6c596329cb49ebdb4
boulderboy <gl.andrewmess@gmail.com>
2018-11-14 19:14:41 +0200

    готово

--- gulpTraining/gulpfile.js
+++ gulpTraining/gulpfile.js
@@ -3,6 +3,37 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var gulpautoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('src/js/*.js'),
+            uglify(),
+            gulp.dest('src/js/minijs')
+        ],
+        cb
+    );
+});
+
+gulp.task('browserSync', function () {
+   browserSync({
+       server: {
+           baseDir: 'src'
+       }
+   })
+});
+
+gulp.task('default', () =>
+    gulp.src('src/styles/css/*.+(css)')
+        .pipe(gulpautoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('src/styles/auteprefixcss'))
+);
 
 gulp.task('minify-css', () => {
     return gulp.src('styles/css/*.+(css)')
@@ -13,11 +44,13 @@ gulp.task('minify-css', () => {
 gulp.task('sass', function () {
     return gulp.src('src/styles/sass/*.+(sass)')
         .pipe(sass())
-        .pipe(gulp.dest('src/styles/css'));
+        .pipe(gulp.dest('src/styles/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch',['browserSync'], function () {
    gulp.watch('src/styles/sass/*.+(sass)', ['sass']);
+   gulp.watch('src/index.html', browserSync.reload)
 });
 
 gulp.task('css', function () {
